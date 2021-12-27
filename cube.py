1a69a3293851f57395e3ab2a4848a17e6cd2c285
Emmett Breen <ejbreen32@gmail.com>
2021-12-27 18:07:20 -0500

    orientation for corners

--- cube.py
+++ cube.py
@@ -1,28 +1,37 @@
 ## RUBIX SIMULATOR
 
+# position
+
 corner_cubies = []
 corner_cubicles = ["flu", "blu", "bru", "fru", "fld", "bld", "brd", "frd"]
 
 edge_cubies = []
 edge_cubicles = ["fu", "lu", "bu", "ru", "fl", "bl", "br", "fr", "fd", "ld", "bd", "rd"]
 
+# orientation
+
+edge_o = []
+corner_o = []
+
+
 ## FUNCTIONS
 
-# rotates cube such that the blue center is in the front
-# convention : blue is front, yellow is up, green is back, white is down, left is orange, right is red
-def rotate_to_f_b(self):
-    if cube['f']['f'] == 'b':
-        return cube
+# returns a dictionary of colors to their desired face
+# a desired face for a color is where that color is the center cubie
+def color_to_face(self):
+    colors = ["b", "w", "y", "g", "r", "o"]
+    faces = ["f", "u", "b", "d", "l", "r"]
+    translator = {}
+    for c in colors:
+        face = ""
+        for f in faces:
+            if cube[f][f] == c:
+                face = f
+        if face = "":
+            raise Exception("Invalid Configuration: no " + c + " face found")
+        translator[c] = face
 
-# returns cubie corresponding to input colors
-# Requires convention: cube['f']['f'] == "b"
-def cubie_of_color(self, colors):
-    cubie = []
-    translator = {"b" : "f", "y" : "u", "g" : "b", "w" : "d", "r" : "r", "o" : "l"}
-    for i in colors:
-        cubie.append(translator[i])
-    return cubie
-    
+
 # uses json input to fetch colors in each cubicle
 def color_in_cubicle(self, cubicle):
     # Returns a list of faces for the current cubicle
@@ -38,16 +47,48 @@ def color_in_cubicle(self, cubicle):
         colors.append(cube[i][cubicle])
     return colors
 
+# returns 0 if first face of cubie (defined by the cubicle) has a desired color
+# returns 1 if second face of cubie (defined by cubicle) has a desired color
+# returns 2 if third face of cubie (defined by cubicle) has a desired color
+# the desired colors are the first faces defined in the cubicle (front and back)
+def corner_orientation(self, colors):
+    for i in range(len(colors)):
+        if colors[i] == cube["f"]["f"] || colors[i] == cube["b"]["b"]
+            return i
 
-## PARSE json file
+    raise Exception("Invalid Configuration: Incorrect corner piece")
+
+def edge_orientation(self, colors):
+    
+
+
+# returns cubie corresponding to input colors
+def cubie_of_color(self, colors):
+    cubie = []
+    translator = color_to_face()
+    for i in colors:
+        cubie.append(translator[i])
+    return cubie
+
+
+## PARSE JSON FILE
 import json
 
 cube = json.load(open('solved.json',))
 
-# Determines corner cubies in corner cubicles
-for cubicle in corner_cubicles:
-    corner_cubies.append(cubie_of_color(color_in_cubicle))
+# Determines corner cubie positions in corner cubicles
 
-# Determines edge cubies in edge cubicles
+for cubicle in corner_cubicles:
+    corner_cubies.append(cubie_of_color(color_in_cubicle(cubicle)))
+    corner_o.append(corner_orientation(color_in_cubicle(cubicle)))
+
+# Determines edge cubie positions in edge cubicles
 for cubicle in edge_cubicles:
-    edge_cubies.append(cubie_of_color(color_in_cubicle))
+    edge_cubies.append(cubie_of_color(color_in_cubicle(cubicle)))
+    edge_cubies.append(edge_orientation(color_in_cubicle(cubicle)))
+
+
+## TODO: VALID CONFIGURATIONS
+
+
+## TODO: ROTATIONS
